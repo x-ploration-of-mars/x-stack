410171aa8240c3a368f85867a1c429358293414c
maieulchevalier <maieul.chevalier@gmail.com>
2023-07-11 17:41:05 +0200

    fix ssr signin - add nav()

--- src/components/auth/signin.tsx
+++ src/components/auth/signin.tsx
@@ -1,12 +1,18 @@
 import { component$ } from "@builder.io/qwik";
-import { Form } from "@builder.io/qwik-city";
+import {
+  Form,
+  globalAction$,
+  routeAction$,
+  useNavigate,
+} from "@builder.io/qwik-city";
 import { useAuthSignin } from "~/routes/plugin@auth";
 
 export default component$(() => {
   const signIn = useAuthSignin();
+  const nav = useNavigate();
 
   return (
-    <Form action={signIn}>
+    <Form action={signIn} onSubmitCompleted$={() => nav()}>
       <input type="hidden" name="providerId" value="discord" />
       <input
         type="hidden"
--- src/components/auth/signout.tsx
+++ src/components/auth/signout.tsx
@@ -1,11 +1,12 @@
 import { component$ } from "@builder.io/qwik";
-import { Form } from "@builder.io/qwik-city";
+import { Form, useNavigate } from "@builder.io/qwik-city";
 import { useAuthSignout } from "~/routes/plugin@auth";
 
 export default component$(() => {
   const signOut = useAuthSignout();
+  const nav = useNavigate();
   return (
-    <Form action={signOut}>
+    <Form action={signOut} onSubmitCompleted$={() => nav()}>
       <input type="hidden" name="callbackUrl" value="/" />
       <button>Sign Out</button>
     </Form>
