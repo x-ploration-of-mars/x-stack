21d764db70b83b31aba71078f9db3b9fca3bd1c7
maieulchevalier <maieul.chevalier@gmail.com>
2023-08-04 17:17:25 +0200

    test drizzle with process.env on vercel #2

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,13 +1,19 @@
+import {
+  type RequestEventAction,
+  type RequestEventLoader,
+} from "@builder.io/qwik-city";
+
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 import { connect } from "@planetscale/database";
 import "dotenv/config";
 
 import * as schema from "~/drizzle/schema";
 
-const connection = connect({
-  host: process.env["DATABASE_HOST"],
-  username: process.env["DATABASE_USERNAME"],
-  password: process.env["DATABASE_PASSWORD"],
-});
-
-export const db = drizzle(connection, { schema });
+export const db = (requestEvent?: RequestEventLoader | RequestEventAction) => {
+  const connection = connect({
+    host: requestEvent?.env.get("DATABASE_HOST"),
+    username: requestEvent?.env.get("DATABASE_USERNAME"),
+    password: requestEvent?.env.get("DATABASE_PASSWORD"),
+  });
+  return drizzle(connection, { schema });
+};
