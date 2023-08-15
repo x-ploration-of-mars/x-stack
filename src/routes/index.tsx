336e24d9a4b0a8ffdaba6f8c87a4fff2206e6cad
maieulchevalier <maieul.chevalier@gmail.com>
2023-08-15 12:09:06 +0200

    clean up

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -48,14 +48,14 @@ export default component$(() => {
               <Signin />
             </div>
             <p class="text-muted-foreground px-8 text-center text-sm">
-              By creating an account, you agree to our{" "}
+              By creating an account, you agree to our
               <Link
                 href="/terms"
                 class="hover:text-primary underline underline-offset-4"
               >
                 Terms of Service
-              </Link>{" "}
-              and{" "}
+              </Link>
+              and
               <Link
                 href="/privacy"
                 class="hover:text-primary underline underline-offset-4"
