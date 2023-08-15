89b8ea2dab9f994e58c0fc8fa4c552e8b4b8fcb0
maieulchevalier <maieul.chevalier@gmail.com>
2023-08-15 19:17:55 +0200

    /: add top ui

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 import { type DocumentHead } from "@builder.io/qwik-city";
+import { Button } from "~/components/ui/button";
 import XGlow from "~/components/x-glow/x-glow";
 
 export default component$(() => {
@@ -8,6 +9,14 @@ export default component$(() => {
       <div class="relative h-[100vh] w-[100vw] flex-col bg-gray-950 text-white">
         <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
           <XGlow />
+        </div>
+        <div class="absolute  top-24 flex w-full justify-around">
+          <div class="  drop-shadow-[0_0_2px_#fff]">
+            <Button variant="ghost">Sign Out</Button>
+          </div>
+          <div class="  drop-shadow-[0_0_2px_#fff]">
+            <Button variant="outline">Sign Out</Button>
+          </div>
         </div>
       </div>
     </>
