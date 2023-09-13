fe64e468cad7e185d831fe0e1d2f2cc265be8b0c
maieulchevalier <maieul.chevalier@gmail.com>
2023-09-13 16:53:22 +0200

    update meta

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -14,6 +14,20 @@ export const RouterHead = component$(() => {
 
       <link rel="canonical" href={loc.url.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://linkfork.co/" />
+      <meta
+        property="og:title"
+        content="LinkFork | Link Preview Customization"
+      />
+      <meta
+        property="og:description"
+        content="LinkFork lets you shorten, and customize how your link will appear when shared on social media, for free."
+      />
+      <meta
+        property="og:image"
+        content="https://linkfork.co/images/poster.png"
+      />
       <link rel="icon" type="image/svg+xml" href="/icons/X-light/favicon.ico" />
 
       {head.meta.map((m) => (
