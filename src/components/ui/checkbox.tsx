c217089d62e3b6c64a62ab14ac53b9c5ba351233
maiieul <maieul.chevalier@gmail.com>
2024-01-30 11:46:42 +0100

    fix checkbox after update

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -8,8 +8,8 @@ export default component$<CheckboxProps>(({ ...props }) => {
   return (
     <div>
       <input
-        {...props}
         type="checkbox"
+        {...props}
         class={cn(
           "peer h-4 w-4 shrink-0 rounded border border-primary text-primary ring-offset-background focus:ring-ring focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
           props.class
